656d9597e9fe4ca6fe7073f48ccc4e723b8c20c0
zoripong <dbfl0406@e-mirim.hs.kr>
2018-05-30 19:51:52 +0900

    done [abandon pet part]

--- WebContent/O3618/o3618.js
+++ WebContent/O3618/o3618.js
@@ -1,36 +1,48 @@
-/* global variable */
+/* [global variable] */
 var g_hospital_list = [];
 var g_hospital_start_index = 0;
-/* end of global variable */
+
+var is_abandon_load = false;
+var g_abandon_list = [];
+
+var g_selling_pet_list = [];
+var g_selling_start_index = 0;
+
+var g_walking_route_list = [];
+var g_walking_start_index = 0;
+/* [end of global variable] */
 
 window.onload = init;
 
 function init() {
 	//		handleRefresh();
-	updateURL(1, 100); //주소 업데이트
+	initHandler();
+	loadMap();
+	updateHospitalApi(1, 100);
+	updateAbandonPetApi();
+	updateSellingPetApi();
+	updateWalkingRouteApi();
 }
 
 // [about map]
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-mapOption = {
-	center : new daum.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-	level : 3
-// 지도의 확대 레벨
-};
-
-var map = new daum.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+function loadMap(){
+	
+	var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+	mapOption = {
+		center : new daum.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+		level : 3
+	// 지도의 확대 레벨
+	};
+	
+	var map = new daum.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+}
 
 // [end of map]
 
 // [about animal hospital api]
-function updateURL(start, end) {
+function updateHospitalApi(start, end) {
 	var url = "http://openapi.seoul.go.kr:8088/4b4b67704d6462663132306c47655155/json/SeoulAnimalHospital/"
 			+ start + "/" + end + "/";
-	/*
-	$.getJSON(url, function(json){
-		alert("success-getAnimalHospital")
-	});
-	 */
 
 	$.ajax({
 		type : "GET",
@@ -58,7 +70,7 @@ function updateURL(start, end) {
 		},
 		error : function(xhr, status, error) {
 			var errorMessage = xhr.status + ': ' + xhr.statusText
-			alert('Error - ' + errorMessage);
+//			alert('Error - ' + errorMessage);
 		}
 	});
 
@@ -72,12 +84,12 @@ function nextPageList() {
 		return;
 	}
 
-	$("#sc_animal_content").empty();
-	$("#sc_animal_content").append("<div class='animal_hospital_row'>");
+	$("#sc_hospital_list").empty();
+	$("#sc_hospital_list").append("<div class='animal_hospital_row'>");
 
 	while (true) {
 		// innerHTML
-		$("#sc_animal_content").append(
+		$("#sc_hospital_list").append(
 				"<div class='animal_hospital_item'><h1>"
 						+ g_hospital_list[g_hospital_start_index].WRKP_NM
 						+ "</h1>"
@@ -93,19 +105,20 @@ function nextPageList() {
 		}
 
 		if (g_hospital_start_index % 4 == 0) {
-			$("#sc_animal_content").append(
+			$("#sc_hospital_list").append(
 					"</div><div class='animal_hospital_row'>");
-			$("#sc_animal_content").append("<br/>");
+			$("#sc_hospital_list").append("<br/>");
 		}
 
 		if (g_hospital_start_index % 8 == 0) {
-			$("#sc_animal_content").append("</div>");
+			$("#sc_hospital_list").append("</div>");
 			break;
 		}
 
 	}
 
 }
+
 function prevPageList() {
 	//alert("hi! :"+ g_hospital_start_index);
 	if (g_hospital_start_index <= 8) {
@@ -116,3 +129,131 @@ function prevPageList() {
 		nextPageList();
 	}
 }
+
+//[end of animal hospital api]
+
+// [about Abandon Pet]
+function updateAbandonPetApi(){
+	var url = "http://openapi.seoul.go.kr:8088/4b4b67704d6462663132306c47655155/json/octastatapi369/1/100/";
+
+	$.ajax({
+		type : "GET",
+		async : true,
+		url : url,
+		dataType : "json",
+		success : function(response, status, result) {
+			var response = JSON.parse(JSON.stringify(response));
+			var octastatapi369 = JSON.parse(JSON.stringify(response.octastatapi369));
+			var abandon_list = JSON.parse(JSON.stringify(octastatapi369.row));
+			
+			for(var i = 0; i<abandon_list.length; i++){
+
+				var keys = Object.keys( abandon_list[i]);
+				
+				for(var j = 0; j<keys.length; j++){
+					if((keys[j] != "JACHIGU")&& (abandon_list[i][keys[j]] == "-"))
+						abandon_list[i][keys[j]] = "0";
+				}
+				
+				
+				g_abandon_list[abandon_list[i].JACHIGU] = abandon_list[i];
+			}
+			//alert(g_abandon_list["종로구"].GIGAN);
+			is_abandon_load = true;
+			setAbandonView($( "#abandon_district" ).val());
+		},
+		error : function(xhr, status, error) {
+			var errorMessage = xhr.status + ': ' + xhr.statusText
+//			alert('Error - ' + errorMessage);
+		}
+	});
+}
+
+
+function setAbandonView(district){
+	$( "#td_district" ).text(district);
+	
+	//td_adoption_value
+	$( "#td_adoption_value" ).text(  parseInt(g_abandon_list[district].IBYANGBUNYANG_1) + parseInt(g_abandon_list[district].IBYANGBUNYANG_2) + parseInt(g_abandon_list[district].IBYANGBUNYANG_3) );
+
+	//td_death_value
+	$( "#td_death_value" ).text( parseInt(g_abandon_list[district].PYESAALLAKSA_1) + parseInt(g_abandon_list[district].PYESAALLAKSA_2) + parseInt(g_abandon_list[district].PYESAALLAKSA_3) );
+
+
+	//td_gyeryugijeung_value
+	$( "#td_gyeryugijeung_value" ).text( parseInt(g_abandon_list[district].GYERYUGIJEUNG_1) + parseInt(g_abandon_list[district].GYERYUGIJEUNG_2) + parseInt(g_abandon_list[district].GYERYUGIJEUNG_3) );
+	
+	//td_indojuin_value
+	$( "#td_indojuin_value" ).text(  parseInt(g_abandon_list[district].INDOJUIN_1) + parseInt(g_abandon_list[district].INDOJUIN_2) + parseInt(g_abandon_list[district].INDOJUIN_3)  );
+	
+	$( "#td_total_value" ).text(  g_abandon_list[district].HAPGYE );
+}
+	
+// [end of Abandon Pet]
+
+// [about selling Pet]
+function updateSellingPetApi(){
+	var url = "http://openapi.seoul.go.kr:8088/4b4b67704d6462663132306c47655155/json/ListLocaldata131501P/1/500/";
+
+	$.ajax({
+		type : "GET",
+		async : true,
+		url : url,
+		dataType : "json",
+		success : function(response, status, result) {
+			var response = JSON.parse(JSON.stringify(response));
+			var ListLocaldata131501P = JSON.parse(JSON.stringify(response.ListLocaldata131501P));
+			var selling_pet_list = JSON.parse(JSON.stringify(ListLocaldata131501P.row));
+			
+			var count = 0;
+			for(var i = 0; i<selling_pet_list.length; i++){
+				if(selling_pet_list[i].trdStateNm == "운영중"){
+					g_selling_pet_list[count++] = selling_pet_list[i];
+				}
+			}
+			
+		},
+		error : function(xhr, status, error) {
+			var errorMessage = xhr.status + ': ' + xhr.statusText
+//			alert('Error - ' + errorMessage);
+		}
+	});
+}
+// [end of selling Pet]
+
+// [about walking route]
+function updateWalkingRouteApi(){
+//	alert("updateWalkingRouteApi");
+	var url = "http://openapi.seoul.go.kr:8088/4b4b67704d6462663132306c47655155/json/GeoInfoWalkwayWGS/1/100";
+
+	$.ajax({
+		type : "GET",
+		async : true,
+		url : url,
+		dataType : "json",
+		success : function(response, status, result) {
+			var response = JSON.parse(JSON.stringify(response));
+			var GeoInfoWalkwayWGS = JSON.parse(JSON.stringify(response.GeoInfoWalkwayWGS));
+			var walk_route_list = JSON.parse(JSON.stringify(GeoInfoWalkwayWGS.row));
+			
+			for(var i = 0; i<walk_route_list.length; i++){
+				g_walking_route_list[i] = walk_route_list[i];
+			}
+//			alert(g_walking_route_list[0].OBJECTID);
+		},
+		error : function(xhr, status, error) {
+			var errorMessage = xhr.status + ': ' + xhr.statusText
+//			alert('Error - ' + errorMessage);
+		}
+	});
+}
+// [end of walking route]
+
+function initHandler(){
+	$( "#abandon_district" ).change(function() {
+		//alert($( "#abandon_district" ).val());
+		  if(is_abandon_load)
+			  setAbandonView($( "#abandon_district" ).val());
+	});
+}
+
